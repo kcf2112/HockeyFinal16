869dca1ae16ce614393524c507dbcc558246b788
Kevin Filer <kcf2112@gmail.com>
2025-04-03 16:56:56 -0400

    Initial Commit

---
+++ HockeyFinal16/HockeyFinal16App.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyFinal16App.swift
+//  HockeyFinal16
+//
+//  Created by Kevin Filer on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct HockeyFinal16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HockeyFinal16Tests/HockeyFinal16Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyFinal16Tests.swift
+//  HockeyFinal16Tests
+//
+//  Created by Kevin Filer on 4/3/25.
+//
+
+import Testing
+@testable import HockeyFinal16
+
+struct HockeyFinal16Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
