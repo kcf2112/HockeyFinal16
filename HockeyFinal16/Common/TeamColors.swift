234f4c2d61f312eb0520659cdee2fcb4aba4ef77
Kevin Filer <kcf2112@gmail.com>
2025-04-16 18:49:57 -0400

    Updated custom color initializers.

--- HockeyFinal16/Common/TeamColors.swift
+++ HockeyFinal16/Common/TeamColors.swift
@@ -225,49 +225,46 @@ enum TeamColors: String, Hashable, CaseIterable
                 return .silver
         }
     }
+    var textColor: Color
+    {
+        switch self
+        {
+            case .ANA, .BOS, .CHI, .LAK, .PIT, .VGK:
+                return .black
+            default:
+                return .white
+        }
+    }
 }
 
 extension Color
 {
-    // static let forestGreen = Color(UIColor(red: 21, green: 71, blue: 52, alpha: 1))
-    static let forestGreen = Color(red: 21/255, green: 71/255, blue: 52/255)
-    // static let metallicGold = Color(UIColor(red: 181, green: 152, blue: 90, alpha: 1))
-    static let metallicGold = Color(red: 181/255, green: 152/255, blue: 90/255)
-    // static let brickRed = Color(UIColor(red: 140, green: 38, blue: 51, alpha: 1))
-    static let brickRed = Color(red: 140, green: 38, blue: 51)
-    // static let navyBlue = Color(UIColor(red: 0, green: 38, blue: 84, alpha: 1))
-    static let navyBlue = Color(red: 0, green: 38/255, blue: 84/255)
-    static let boundlessBlue = Color(red: 53/255, green: 84/255, blue: 100/255)
-    // static let gold = Color(UIColor(red: 204, green: 138, blue: 0, alpha: 1))
-    static let gold = Color(red: 204/255, green: 138/255, blue: 0)
-    // static let burgundy = Color(UIColor(red: 111, green: 38, blue: 61, alpha: 1))
-    static let burgundy = Color(red: 111/255, green: 38/255, blue: 61/255)
-    // static let victoryGreen = Color(UIColor(red: 0, green: 99, blue: 65, alpha: 1))
-    static let victoryGreen = Color(red: 0, green: 99/255, blue: 65/255)
-    // static let navy = Color(UIColor(red: 4, green: 30, blue: 66, alpha: 1))
-    static let navy = Color(red: 4/255, green: 30/255, blue: 66/255)
-    // static let silver = Color(UIColor(red: 162, green: 170, blue: 173, alpha: 1))
-    static let silver = Color(red: 162/255, green: 170/255, blue: 173/255)
-    // static let royalBlue = Color(UIColor(red: 0, green: 48, blue: 135, alpha: 1))
-    static let royalBlue = Color(red: 0, green: 48/255, blue: 135/255)
-    // static let pittsburghGold = Color(UIColor(red: 181, green: 152, blue: 90, alpha: 1))
-    static let pittsburghGold = Color(red: 181/255, green: 152/255, blue: 90/255)
     static let bostonGold = Color(red: 252/255, green: 181/255, blue: 20/255)
-    static let iceBlue = Color(UIColor(red: 153, green: 217, blue: 217, alpha: 1))
-    static let deepPacificTeal = Color(UIColor(red: 0, green: 98, blue: 114, alpha: 1))
-    // static let tampaBayBlue = Color(UIColor(red: 0, green: 32, blue: 91, alpha: 1))
-    static let tampaBayBlue = Color(red: 0, green: 32/255, blue: 91/255)
-    // static let polarNightBlue = Color(UIColor(red: 4, green: 30, blue: 66, alpha: 1))
-    static let polarNightBlue = Color(red: 4/255, green: 30/255, blue: 66/255)
-    static let desertSand = Color(UIColor(red: 226, green: 214, blue: 181, alpha: 1))
-    static let ironRangeRed = Color(UIColor(red: 166, green: 25, blue: 46, alpha: 1))
-    static let deepSeaBlue = Color(UIColor(red: 0, green: 22, blue: 40, alpha: 1))
-    static let burntOrange = Color(UIColor(red: 229, green: 114, blue: 0, alpha: 1))
+    static let boundlessBlue = Color(red: 53/255, green: 84/255, blue: 100/255)
+    static let brickRed = Color(red: 140, green: 38, blue: 51)
+    static let burgundy = Color(red: 111/255, green: 38/255, blue: 61/255)
+    static let burntOrange = Color(red: 229/255, green: 114/255, blue: 0)
+    static let coolGray = Color(red: 151/255, green: 153/255, blue: 155/255)
+    static let darkGray = Color(red: 124/255, green: 135/255, blue: 142/255)
+    static let deepPacificTeal = Color(red: 0, green: 98/255, blue: 114/255)
+    static let deepSeaBlue = Color(red: 0, green: 22/255, blue: 40/255)
+    static let desertSand = Color(red: 226/255, green: 214/255, blue: 181/255)
+    static let flatGold = Color(red: 185/255, green: 151/255, blue: 91/255)
+    static let flatSilver = Color(red: 162/255, green: 170/255, blue: 173/255)
     static let flyersOrange = Color(red: 247/255, green: 73/255, blue: 2/255)
-    static let flatSilver = Color(UIColor(red: 162, green: 170, blue: 173, alpha: 1))
-    static let flatGold = Color(UIColor(red: 185, green: 151, blue: 91, alpha: 1))
-    static let harvestGold = Color(UIColor(red: 234, green: 170, blue: 0, alpha: 1))
-    static let darkGray = Color(UIColor(red: 124, green: 135, blue: 142, alpha: 1))
-    static let coolGray = Color(UIColor(red: 151, green: 153, blue: 155, alpha: 1))
-    static let steelGray = Color(UIColor(red: 51, green: 63, blue: 72, alpha: 1))
+    static let forestGreen = Color(red: 21/255, green: 71/255, blue: 52/255)
+    static let gold = Color(red: 204/255, green: 138/255, blue: 0)
+    static let harvestGold = Color(red: 234/255, green: 170/255, blue: 0)
+    static let iceBlue = Color(red: 153/255, green: 217/255, blue: 217/255)
+    static let ironRangeRed = Color(red: 166/255, green: 25/255, blue: 46/255)
+    static let metallicGold = Color(red: 181/255, green: 152/255, blue: 90/255)
+    static let navy = Color(red: 4/255, green: 30/255, blue: 66/255)
+    static let navyBlue = Color(red: 0, green: 38/255, blue: 84/255)
+    static let pittsburghGold = Color(red: 181/255, green: 152/255, blue: 90/255)
+    static let polarNightBlue = Color(red: 4/255, green: 30/255, blue: 66/255)
+    static let royalBlue = Color(red: 0, green: 48/255, blue: 135/255)
+    static let silver = Color(red: 162/255, green: 170/255, blue: 173/255)
+    static let steelGray = Color(red: 51/255, green: 63/255, blue: 72/255)
+    static let tampaBayBlue = Color(red: 0, green: 32/255, blue: 91/255)
+    static let victoryGreen = Color(red: 0, green: 99/255, blue: 65/255)
 }
